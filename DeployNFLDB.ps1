768ebc441eba689b55c71ad9af0ddbac0febd55f
Dubl <rmiddlehouse@hotmail.com>
2019-09-17 20:26:07 -0500

    move schema sql to github

--- DeployNFLDB.ps1
+++ DeployNFLDB.ps1
@@ -4,14 +4,14 @@ $SQLServername=     $args[2]
 $SQLPassword=   $args[3]
 $StorageAcctName=   $args[4]
 $FunctionAppName=   $args[5]
-#./DeployNFLDB.ps1 nfldb01.database.windows.net pass NFLTemp https://nflgenstoragers.blob.core.windows.net
+
 
 #TODO Add deployment JSON code here
 az group create --name $ResourceGroupName --location $Location
 az group deployment create --name NFLDBDeployment --resource-group $ResourceGroupName  --template-uri "https://raw.githubusercontent.com/ml-systems/nfldb/master/mlsystemsnfldbtemplate.json"  --parameters dbserver_name=$SQLServername FunctionApp_name=$FunctionAppName storageAccount_name=$StorageAcctName dbpassword=$SQLPassword
 
 #Download Powershell Script file and execute (this can be changed to github)
-Invoke-WebRequest -Uri "https://nflgenstorage.blob.core.windows.net/public/script.sql" -OutFile "script.sql"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ml-systems/nfldb/master/script.sql" -OutFile "script.sql"
 
 Write-Output "Downloaded NFLDB Schema"
 Write-Output "Deploying NFLDB Schema to nfldb"
